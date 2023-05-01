b0f9e8e00f51047015fa8439f8bed42cef03694a
Maksim L <maksim.lopatin.spb@gmail.com>
2023-05-02 01:34:07 +0300

    генерация контроллера work

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,20 @@
 Rails.application.routes.draw do
+
   resources :themes
   resources :images
   resources :values
   resources :users
-  root 'main#index'
+  root 'work#index'
   get 'main/index'
   get 'main/help'
   get 'main/contacts'
   get 'main/about'
+
+
+  # work
+  match 'work',             to: 'work#index',            via: 'get'
+  match 'choose_theme',     to: 'work#choose_theme',      via: :get
+  match 'display_theme',    to: 'work#display_theme',      via: :post
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
